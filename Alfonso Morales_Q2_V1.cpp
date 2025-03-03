9a27edf87807101d6134fa9359e5eff10500b7ba
mousebytes <alfonso.morales625625@gmail.com>
2025-03-03 00:46:31 -0800

    Initial commit: Added C++ file

---
+++ Alfonso Morales_Q2_V1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+// not using namespace std just for practice
+
+void cosine_similarity(); // A dot B = ||A|| * ||B|| * cos(theta)
+void levenshtein_distance();
+void jaccard_similarity();
+
+
+
+int main(void)
+{
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+void cosine_similarity()
+{
+
+}
+
+void levenshtein_distance()
+{
+
+}
+
+void jaccard_similarity()
+{
+
+}
