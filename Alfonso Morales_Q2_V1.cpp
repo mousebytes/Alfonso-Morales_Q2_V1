44470c241b6a76b795345f5b8224a42221aba53b
mousebytes <alfonso.morales625625@gmail.com>
2025-03-03 02:38:10 -0800

    test1

--- Alfonso Morales_Q2_V1.cpp
+++ Alfonso Morales_Q2_V1.cpp
@@ -1,17 +1,33 @@
 #include<iostream>
+#include<fstream>
+#include<unordered_map>
+#include<cmath>
+#include<vector>
+#include<string>
 // not using namespace std just for practice
 
+
+// cosine similarity prototypes
 void cosine_similarity(); // A dot B = ||A|| * ||B|| * cos(theta)
+std::string normalize_words(std::string word);
+
+
+// levenshtein distance prototypes
 void levenshtein_distance();
+
+// jaccard similarity prototypes
 void jaccard_similarity();
+
+
+
 
 
 
 int main(void)
 {
-
-
-
+    std::string word = "Hello world";
+    std::cout << normalize_words(word) << "\n";
+    
 
 
 
@@ -20,17 +36,41 @@ int main(void)
     return 0;
 }
 
+
+
+
+
+
+
+
+// cosine similarity implementations
 void cosine_similarity()
 {
 
 }
+std::string normalize_words(std::string word) // normalize a word for the unordered_map key
+{
+    std::string normalized;
 
+    for(char c : word ) // for every char in passed word string
+    {
+        if(isalpha(c)) // check if it's a letter
+            normalized += tolower(c); // move to lowercase
+    }
+
+    return normalized;
+}
+
+
+// jaccard similarity implementations
+void jaccard_similarity()
+{
+
+}
+
+// levenshtein distance implementations
 void levenshtein_distance()
 {
 
 }
 
-void jaccard_similarity()
-{
-
-}
